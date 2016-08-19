3de2b62c86d12317ddf58183e7f7186d9c9cbfc0
Samridhi Dubey <sd77574@gmail.com>
2016-08-19 14:57:52 +0530

    Added initial project files

---
+++ src/javalearning/FirstLearningClass.java
@@ -0,0 +1,25 @@
+package javalearning;
+
+public class FirstLearningClass {
+    public static void main(String[] args) {
+        Sum();
+        Minus();
+        System.out.println("Hello World");
+    }
+
+    public static void Sum() {
+        int a = 20;
+        int b = 30;
+        int s;
+        s = a + b;
+        System.out.println(s);
+    }
+
+    public static void Minus() {
+        int a = 20;
+        int b = 30;
+        int s;
+        s = a - b;
+        System.out.println(s);
+    }
+}
