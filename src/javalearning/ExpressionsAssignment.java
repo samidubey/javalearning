c0400c4002bde77f1d57bda434ae7105685aff1a
samridhi_dubey <sd77574@gmail.com>
2016-08-19 17:07:26 +0530

    Completed a left expression!

--- src/javalearning/ExpressionsAssignment.java
+++ src/javalearning/ExpressionsAssignment.java
@@ -11,15 +11,15 @@ public class ExpressionsAssignment {
     	int a = 10;
     	int b = 5;
     	int result1;
-    	// boolean result2;
+    	boolean result2;
     	int result3;
     	int result4;
     	
         result1 = (a<<2)+(b>>2);
         System.out.println(result1);
         
-       // result2 = ((a) || (b > 0));
-      //  System.out.println(result2);
+        result2 = ((a!=0) || (b > 0));
+        System.out.println(result2);
         
         result3 = (a + b * 100)/10;
         System.out.println(result3);
