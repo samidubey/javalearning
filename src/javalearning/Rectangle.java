98e35dda5b68c9588c3d351624bdcda07c930159
samridhi_dubey <sd77574@gmail.com>
2016-08-19 16:14:48 +0530

    Rectangle exercise Done :)

---
+++ src/javalearning/Rectangle.java
@@ -0,0 +1,18 @@
+package javalearning;
+
+public class Rectangle {
+	public static void main(String[] args)
+	{
+		rectangleArea();
+	}
+	public static void rectangleArea()
+	{
+		int height = 10;
+		int width = 20;
+		int area;
+		area = height * width;
+		System.out.println(area);
+	}
+	
+
+}
