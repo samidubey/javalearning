7abd24d2f15923efd3b20ded74bc20cfb3003f07
samridhi_dubey <sd77574@gmail.com>
2016-08-25 17:33:27 +0530

    Printing Numbers example

---
+++ src/javalearning/PrintUsingFor.java
@@ -0,0 +1,20 @@
+package javalearning;
+
+public class PrintUsingFor {
+	public static void main(String[] args)
+	{
+		PrintUsingFor();
+	}
+	public static void PrintUsingFor()
+	{
+	  for (int i=1;i<=5; i++)
+	  {
+		  for (int j=1;j<=i;j++)
+		  {
+			  System.out.print(j+" ");		 
+		  }
+		System.out.println("");
+	  }
+	 }
+	
+}
