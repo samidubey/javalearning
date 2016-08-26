e62cba1c6731af8a937079079f42ef6a54aa0443
samridhi_dubey <sd77574@gmail.com>
2016-08-26 18:02:30 +0530

    updated

--- src/javalearning/RectangleDemo.java
+++ src/javalearning/RectangleDemo.java
@@ -7,14 +7,20 @@ public static void main(String[] args)
 	Rectangle rect = new Rectangle( 4, 5,  "White");
 
     Rectangle rect1 = new Rectangle(4, 6, "White");
-    
-    rect1.equals(rect1,rect);
-    rect.equals(rect1,rect);
     boolean result = rect1.equals(rect, rect1);
-    boolean result1 =   rect1.equals(rect1,rect);
+
 	
-	System.out.println(result);
-	System.out.println(result1);
-	System.out.println(rect.area());
+	System.out.println("Rectangle 1 - Length[" + rect.getLength()+"], Width["+rect.getWidth()+"], Color["+rect.getColour()+"]");
+	System.out.println("Rectangle 2 - Length[" + rect1.getLength()+"], Width["+rect1.getWidth()+"], Color["+rect1.getColour()+"]");
+
+	if (result == true)
+	{
+		System.out.println("Compared Rectangle 1 and Rectangle 2 - Match");
+	}
+	else
+	{
+		System.out.println("Compared Rectangle 1 and Rectangle 2 - Mismatch");
+	}
+
 }
 }
