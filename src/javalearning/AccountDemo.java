2d401fc27a862ae9fd8c1fd5000f63098aa11f08
samridhi_dubey <sd77574@gmail.com>
2016-08-30 14:38:24 +0530

    Updated

--- src/javalearning/AccountDemo.java
+++ src/javalearning/AccountDemo.java
@@ -5,10 +5,11 @@ public class AccountDemo {
 	public static void main(String args[]) {
 		Account acc = new Account("Sam", 1234, "Hyderabad", "Saving", 25000);
 		Account acc1 = new Account("Sam", 1234, 25);
-		System.out.println(acc.getBalance());
-		System.out.println(acc1.getBalance());
+		System.out.printf("First Method Current Balance - %.0f\n", acc1.getBalance());
+		System.out.printf("Second Method Current Balance - %.0f\n", acc.getBalance());
 
-		System.out.println("Current Balance - " + acc.deposit(500));
-		System.out.println("Current Balance - " + acc.withdraw(500));
+		System.out.printf("Current Balance - %.0f\n", acc.deposit(500));
+
+		System.out.printf("Current Balance - %.0f\n", acc.withdraw(500));
 	}
 }
