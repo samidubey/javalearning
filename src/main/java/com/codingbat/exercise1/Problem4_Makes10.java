3d449b6e06397390e53e5d92c1dfa0ee9989d983
Samridhi Dubey <sd77574@gmail.com>
2017-01-24 16:45:20 +0530

    Problem4 makes10 java and test files

--- src/main/java/com/codingbat/exercise1/Problem4_Makes10.java
+++ src/main/java/com/codingbat/exercise1/Problem4_Makes10.java
@@ -2,4 +2,16 @@ package com.codingbat.exercise1;
 
 public class Problem4_Makes10 {
 
+    public boolean makes10(int a, int b) {
+        return (a == 10 || b == 10 || (a + b == 10));
+
+    }
+
+    public static void main(String[] args) {
+        Problem4_Makes10 obj = new Problem4_Makes10();
+        System.out.println(obj.makes10(9, 10));
+        System.out.println(obj.makes10(9, 9));
+        System.out.println(obj.makes10(1, 9));
+    }
+
 }
