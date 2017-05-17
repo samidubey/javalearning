d9d43aea1df93f207973e06f29bbfbaa2b33bf5d
Samridhi Dubey <sd77574@gmail.com>
2017-05-17 10:54:12 +0200

    GH-11:codingbat front3 test file added

--- src/main/java/com/codingbat/exercise1/Problem12_Front3.java
+++ src/main/java/com/codingbat/exercise1/Problem12_Front3.java
@@ -3,49 +3,52 @@ package com.codingbat.exercise1;
 public class Problem12_Front3 {
 
 	public static void main(String[] args) {
+		Problem12_Front3 obj = new Problem12_Front3();
 		// TODO Auto-generated method stub
-		System.out.println(front3("java"));
-		System.out.println(front3("chocolate"));
-		System.out.println(front3("abc"));
-		System.out.println(front3(""));
-		System.out.println(front3("ab"));
-		System.out.println(front3("a"));
+		System.out.println(obj.front3("java"));
+		System.out.println(obj.front3("chocolate"));
+		System.out.println(obj.front3("abc"));
+		System.out.println(obj.front3(""));
+		System.out.println(obj.front3("ab"));
+		System.out.println(obj.front3("a"));
 
-		System.out.println(front3OldSchool("Deepak"));
-		System.out.println(front3OldSchool("Dee"));
-		System.out.println(front3OldSchool(""));
-		System.out.println(front3OldSchool("bc"));
-		System.out.println(front3OldSchool("b"));
+		System.out.println(obj.front3OldSchool("Deepak"));
+		System.out.println(obj.front3OldSchool("Dee"));
+		System.out.println(obj.front3OldSchool(""));
+		System.out.println(obj.front3OldSchool("bc"));
+		System.out.println(obj.front3OldSchool("b"));
 
 	}
 
-	public static String front3(String str) {
+	public String front3(String str) {
 
-		if (str != null && !str.isEmpty()) {
-			StringBuilder sb = new StringBuilder(str);
-			if (sb.length() >= 3) {
-				sb.delete(3, sb.length());
-				String firstThreeChar = sb.toString();
-				sb.insert(0, firstThreeChar);
-				String output = sb.toString() + firstThreeChar;
-				return output;
-			}
-			return sb.toString() + sb.toString() + sb.toString();
+		if (str == null || str.isEmpty()) {
+			return "";
 		}
-		return str;
+		StringBuilder sb = new StringBuilder(str);
+		if (sb.length() >= 3) {
+			sb.delete(3, sb.length());
+			String firstThreeChar = sb.toString();
+			sb.insert(0, firstThreeChar);
+			String output = sb.toString() + firstThreeChar;
+			return output;
+		}
+		return sb.toString() + sb.toString() + sb.toString();
+
 	}
 
-	public static String front3OldSchool(String str) {
+	public String front3OldSchool(String str) {
 
-		if (str != null && !str.isEmpty()) {
-			if (str.length() >= 3) {
-				String firstThreeChar = str.substring(0, 3);
-				String output = firstThreeChar + firstThreeChar + firstThreeChar;
-				return output;
-			}
-			return str + str + str;
+		if (str == null || str.isEmpty()) {
+			return "";
 		}
-		return str;
+		if (str.length() >= 3) {
+			String firstThreeChar = str.substring(0, 3);
+			String output = firstThreeChar + firstThreeChar + firstThreeChar;
+			return output;
+		}
+		return str + str + str;
+
 	}
 
 }
---
+++ src/test/java/com/codingbat/exercise1/Problem12_Front3Test.java
@@ -0,0 +1,50 @@
+package com.codingbat.exercise1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Problem12_Front3Test {
+	private Problem12_Front3 fixture;
+
+	@Before
+	public void setUp() {
+		fixture = new Problem12_Front3();
+	}
+
+	@Test
+	public void testCaseThreeFrontCharacterTakenAndRepitatedThrice() {
+		String str = "java";
+		String expected = "javjavjav";
+		String actual1 = fixture.front3(str);
+		String actual2 = fixture.front3OldSchool(str);
+		assertThat(actual1, is(equalTo(expected)));
+		assertThat(actual2, is(equalTo(expected)));
+
+	}
+
+	@Test
+	public void testCaseWhenStrIsNull() {
+		String str = null;
+		String expected = "";
+		String actual1 = fixture.front3(str);
+		String actual2 = fixture.front3OldSchool(str);
+		assertThat(actual1, is(equalTo(expected)));
+		assertThat(actual2, is(equalTo(expected)));
+
+	}
+
+	@Test
+	public void testCaseWhenStrIsEmpty() {
+		String str = "";
+		String expected = "";
+		String actual1 = fixture.front3(str);
+		String actual2 = fixture.front3OldSchool(str);
+		assertThat(actual1, is(equalTo(expected)));
+		assertThat(actual2, is(equalTo(expected)));
+
+	}
+}
