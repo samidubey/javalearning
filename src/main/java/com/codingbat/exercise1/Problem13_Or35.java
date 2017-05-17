12d3fdab303086e4798bfdecbd8d595f920a7a64
Samridhi Dubey <sd77574@gmail.com>
2017-05-17 12:54:25 +0200

    GH-13:codingbat or35 java and test file added

---
+++ src/main/java/com/codingbat/exercise1/Problem13_Or35.java
@@ -0,0 +1,25 @@
+package com.codingbat.exercise1;
+
+public class Problem13_Or35 {
+	public static void main(String[] args) {
+		Problem13_Or35 obj = new Problem13_Or35();
+		System.out.println(obj.or35(3));
+		System.out.println(obj.or35(10));
+		System.out.println(obj.or35(8));
+		System.out.println(obj.or35(22));
+		System.out.println(obj.or35(0));
+		System.out.println(obj.or35(-1));
+
+	}
+
+	public boolean or35(int n) {
+		if (n > 0) {
+			if (n % 3 == 0 || n % 5 == 0) {
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+
+}
