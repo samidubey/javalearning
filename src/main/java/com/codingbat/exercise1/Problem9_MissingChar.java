7b4149024c1df362bd95b3ce2511ca5abac42edc
Samridhi Dubey <sd77574@gmail.com>
2017-05-17 09:47:12 +0200

    GH-8:Junit class file added

--- src/main/java/com/codingbat/exercise1/Problem9_MissingChar.java
+++ src/main/java/com/codingbat/exercise1/Problem9_MissingChar.java
@@ -3,23 +3,28 @@ package com.codingbat.exercise1;
 public class Problem9_MissingChar {
 
 	public static void main(String[] args) {
-		System.out.println(missingChar("Kitten", 0));
-		System.out.println(missingChar("Kitten", 1));
-		System.out.println(missingChar("Kitten", 4));
-		System.out.println(missingChar("Kitten", 5));
-		System.out.println(missingChar("Kitten", 6));
+		Problem9_MissingChar obj = new Problem9_MissingChar();
+
+		System.out.println(obj.missingChar("Kitten", 0));
+		System.out.println(obj.missingChar("Kitten", 1));
+		System.out.println(obj.missingChar("Kitten", 4));
+		System.out.println(obj.missingChar("Kitten", 5));
+		System.out.println(obj.missingChar("Kitten", 6));
 		System.out.println("Kitten".length());
-		System.out.println(missingCharOldSchool("Kitten", 4));
-		System.out.println(missingCharOldSchool("Kitten", 5));
-		System.out.println(missingCharOldSchool("Kitten", 6));
-		System.out.println(missingCharPatiOldSchool("Kitten", 4));
-		System.out.println(missingCharPatiOldSchool("Kitten", 5));
-		System.out.println(missingCharPatiOldSchool("Kitten", 6));
+		System.out.println(obj.missingCharOldSchool("Kitten", 4));
+		System.out.println(obj.missingCharOldSchool("Kitten", 5));
+		System.out.println(obj.missingCharOldSchool("Kitten", 6));
+		System.out.println(obj.missingCharPatiOldSchool("Kitten", 4));
+		System.out.println(obj.missingCharPatiOldSchool("Kitten", 5));
+		System.out.println(obj.missingCharPatiOldSchool("Kitten", 6));
 	}
 
-	public static String missingChar(String str, int n) {
-		StringBuilder sb = new StringBuilder(str);
-		if (n <= str.length() - 1) {
+	public String missingChar(String str, int n) {
+		if (str == null || str.isEmpty()) {
+			return "";
+		}
+		if (n >= 0 && n <= str.length() - 1) {
+			StringBuilder sb = new StringBuilder(str);
 			sb.deleteCharAt(n);
 			String output = sb.toString();
 			return output;
@@ -27,8 +32,11 @@ public class Problem9_MissingChar {
 		return str;
 	}
 
-	public static String missingCharOldSchool(String str, int n) {
-		if (n <= str.length() - 1) {
+	public String missingCharOldSchool(String str, int n) {
+		if (str == null || str.isEmpty()) {
+			return "";
+		}
+		if (n >= 0 && n <= str.length() - 1) {
 			String front = str.substring(0, n);
 			String Back = str.substring(n + 1, str.length());
 			String output = front + Back;
@@ -38,9 +46,12 @@ public class Problem9_MissingChar {
 
 	}
 
-	public static String missingCharPatiOldSchool(String str, int n) {
+	public String missingCharPatiOldSchool(String str, int n) {
+		if (str == null || str.isEmpty()) {
+			return "";
+		}
 		String output = "";
-		if (n <= str.length() - 1) {
+		if (n >= 0 && n <= str.length() - 1) {
 			for (int i = 0; i <= str.length() - 1; i++) {
 				if (i != n) {
 					output = output + String.valueOf(str.charAt(i));
