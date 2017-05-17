905e6ae4f359f3ae0c4c489afa125da8b54cf7ab
Samridhi Dubey <sd77574@gmail.com>
2017-05-17 12:59:27 +0200

    GH-13: updated the code to avoid many if loops

--- src/main/java/com/codingbat/exercise1/Problem13_Or35.java
+++ src/main/java/com/codingbat/exercise1/Problem13_Or35.java
@@ -13,11 +13,8 @@ public class Problem13_Or35 {
 	}
 
 	public boolean or35(int n) {
-		if (n > 0) {
-			if (n % 3 == 0 || n % 5 == 0) {
-				return true;
-			}
-			return false;
+		if (n > 0 && (n % 3 == 0 || n % 5 == 0)) {
+			return true;
 		}
 		return false;
 	}
